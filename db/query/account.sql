cf3739df82ee7df0d38e9e5336a9ab785aa4e2b1
raymondgitonga <raymond@sendyit.com>
2021-08-28 01:35:04 +0300

    Add crud operations for accounts

--- db/query/account.sql
+++ db/query/account.sql
@@ -6,3 +6,21 @@ INSERT INTO accounts (
 ) VALUES (
     $1, $2, $3
 ) RETURNING *;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: UpdateAccount :exec
+UPDATE accounts
+SET balance = $2
+WHERE id = $1;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts where id = $1;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/raymondgitonga/bank_backend_sample
+
+go 1.15
